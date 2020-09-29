d493197b76c689869a967811849156bef215a124
HannahO94 <56817756+HannahO94@users.noreply.github.com>
2020-09-29 09:37:03 +0200

    Updates

---
+++ precache-manifest.5f229e6698b2805ef2329cd31b49f621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a560326d6ecdfef12861ad0da96cc9",
+    "url": "/js-lessons-deplay/index.html"
+  },
+  {
+    "revision": "7de0105aee6886d38f94",
+    "url": "/js-lessons-deplay/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c4ace9f67c7be6add878",
+    "url": "/js-lessons-deplay/static/js/2.c995e2f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-lessons-deplay/static/js/2.c995e2f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de0105aee6886d38f94",
+    "url": "/js-lessons-deplay/static/js/main.65c688f0.chunk.js"
+  },
+  {
+    "revision": "fcb39275813c7f6a2849",
+    "url": "/js-lessons-deplay/static/js/runtime-main.b23b861f.js"
+  }
+]);
